b7ce56d508cb2a2f8cfa57e7e80d5e4a2d282b93
Mishaj__k <amstig100@gmail.com>
2023-12-05 08:31:55 +0530

    feat, fix, re-factor, style:update background colore robo card color

--- src/App.jsx
+++ src/App.jsx
@@ -4,66 +4,89 @@ import './App.css';
 class App extends Component {
   constructor(props) {
     super(props);
+    console.log(props);
 
     this.state = {
       robots: [],
       searchField: '',
-    };
+    }; // state create
+
   }
 
   componentDidMount() {
-    // Fetch users  data from the api
+    // Fetch users data from the api
 
-    fetch('https://jsonplaceholder.typicode.com/users')
+   let a =  fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
-      .then(data => this.setState({ robots: data }))
-      .catch(error => console.error('Error fetching data:', error));
+      .then(data => {
+        //console.log(data)
+        this.setState({ robots: data });
+      })
+      .catch(error => {
+        console.log('Error fetching data:', error);
+      });
+  }
 
-  } //lifecycle methode end here
-
-  
+  handleSearchChange = (event) => {
+    // console.log(event);
+    this.setState({ searchField: event.target.value });
+  };
 
   render() {
     const { robots, searchField } = this.state;
 
     // Filter robots based on the search input
-    const filteredRobots = robots.filter(robot =>
-      robot.name.toLowerCase().includes(searchField.toLowerCase())
-    );
+    const filteredRobots = robots.filter(robot => {
+      let searchfielddata = robot.name.toLowerCase().includes(searchField.toLowerCase());
+      console.log(searchfielddata);
+      return searchfielddata;
+    });
 
     return (
-      <div className="container mt-4">
-        <h1 className="text-center mb-4">Robot Gallery</h1>
+      <>
+        <div className="container mt-4">
 
-        {/* Search input */}
-        <input
-          type="text"
-          className="form-control mb-4"
-          placeholder="Search Robots"
-          onChange={this.handleSearchChange}
-        />
+          <h1 className="text-center mb-4">Robot Application</h1>
 
-        {/* Display robot cards */}
-        <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3">
-          {filteredRobots.map(robot => {
-            return (
-              <div key={robot.id} className="col mb-4">
-                <div className="card">
-                  <img
-                    src={`https://robohash.org/${robot.id}?set=set3`}
-                    alt={robot.name}
-                    className="card-img-top"
-                  />
-                  <div className="card-body">
-                    <h5 className="card-title">{robot.name}</h5>
-                    <p className="card-text">{robot.email}</p>
+          {/* Search input */}
+          <input
+            type="text"
+            className="form-control mb-4"
+            placeholder="Search Robots"
+            onChange={ event => {
+              this.handleSearchChange(event);
+            }} // this is call back that onChange call it self when change make inside the inpu field then event contain value of inside the box what we type then call search function 
+          />
+
+          {/* Display robot cards */}
+          <div className="row">
+            { filteredRobots.map(robot => {
+              return (
+                <div key={robot.id} className="col mb-4">
+                  <div className="card">
+                    <img
+                      src={`https://robohash.org/${robot.id}?set=set3`}
+                      alt={robot.name}
+                      className="card-img-top"
+                    />
+
+                    <div className="card-body">
+                      <h5 className="card-title">{robot.name}</h5>
+                      <p className="card-text">{robot.email}</p> 
+                    </div>
+
                   </div>
+
                 </div>
-              </div>
-            );
-          })}
+              );
+        
+        })}
+        
+          </div>
+        
         </div>
-      </div>
+
+      </>
     );
   }
 }
